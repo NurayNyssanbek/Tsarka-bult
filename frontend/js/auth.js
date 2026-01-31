f4efe9489b143a9b675a4f3cb91c0f2434b66190
NurayNyssanbek <nurainysanbekova@gmail.com>
2026-01-31 12:48:19 +0500

    js

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -7,7 +7,7 @@ const AUTH_USER_KEY = "phishing_training_user";
 
 // Base URL of the backend API (change if you run backend elsewhere)
 // ПРАВИЛЬНЫЙ ВАРИАНТ:
-const API_BASE = "https://tsarka-bult-tsarka-bult-tried.fin1.bult.app/api";
+const API_BASE = "https://tsarka-bult-tsarka-bult-tried.fin1.bult.app//api";
 function getToken() {
   return localStorage.getItem(AUTH_TOKEN_KEY);
 }
